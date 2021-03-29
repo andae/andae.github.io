b7a5145b4cc9c4b6ef684f617cb574c2673790db
Julianda <biwabits@gmail.com>
2021-03-29 15:24:07 +0700

    Initial

---
+++ sw.js
@@ -0,0 +1,21 @@
+var cacheName = 'julianda';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/dist/style/main.css',
+  '/dist/scripts/main.js'
+];
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
